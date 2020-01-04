6a27096d7d63ea4e4464e1c69a6bb84b053665d7
Paweł Bogdan <pawel.bogdan1@gmail.com>
2020-01-04 19:28:03 +0100

    Initial commit

---
+++ src/test/java/pl/altkom/tests/SimpleTest.java
@@ -0,0 +1,12 @@
+package pl.altkom.tests;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class SimpleTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue( true );
+    }
+}
