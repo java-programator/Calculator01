3a19a57a9dad38fce517a87d443a1d276e07b44a
Paweł Bogdan <pawel.bogdan1@gmail.com>
2020-01-04 21:29:48 +0100

    Simple mock test

---
+++ src/test/java/pl/altkom/tests/CalculatorTest.java
@@ -0,0 +1,28 @@
+package pl.altkom.tests;
+
+import org.junit.Test;
+import pl.altkom.tests.aritmetics.FloatArithmetic;
+import pl.altkom.tests.aritmetics.IntegerArithmetic;
+import pl.altkom.tests.exceptions.CalculatorException;
+
+import static org.mockito.Mockito.*;
+
+public class CalculatorTest {
+    @Test
+    public void simpleTest() throws CalculatorException {
+        Calculator c = new Calculator();
+        c.setFloatArithmetic(new FloatArithmetic());
+        c.setIntegerArithmetic(new IntegerArithmetic());
+        Input in = mock(Input.class);
+        c.setIn(in);
+        Output out = mock(Output.class);
+        c.setOut(out);
+
+        when(in.get()).thenReturn('2').thenReturn('+').thenReturn('2').thenReturn('=');
+
+        c.calculateOneOperation();
+
+        verify(out, times(1)).put('4');
+        verify(out, times(1)).put('\n');
+    }
+}
