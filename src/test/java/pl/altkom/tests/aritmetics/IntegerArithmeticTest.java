7c7854ee6b7cae03f4f42a1b7187f9f4f8acbff4
twisz <tomaszwiszynski@gmail.com>
2020-01-05 14:06:45 +0100

    tw branch added, simple integerArithmetic tests added

--- src/test/java/pl/altkom/tests/aritmetics/IntegerArithmeticTest.java
+++ src/test/java/pl/altkom/tests/aritmetics/IntegerArithmeticTest.java
@@ -1,15 +1,57 @@
 package pl.altkom.tests.aritmetics;
 
+import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
+import pl.altkom.tests.Calculator;
 import pl.altkom.tests.StringArithmetic;
 import pl.altkom.tests.exceptions.CalculatorException;
 
 import static org.junit.Assert.*;
 
 public class IntegerArithmeticTest {
+    static StringArithmetic sa;
+
+    @BeforeClass
+    public static void prepare(){
+        sa = new IntegerArithmetic();
+    }
+
+    @Test (expected = CalculatorException.class)
+    public void tesAdditionWithLetters() throws CalculatorException {
+        assertEquals("4", sa.add("2", "dupa"));
+    }
+
     @Test
     public void simpleTest() throws CalculatorException {
-        StringArithmetic sa = new IntegerArithmetic();
         assertEquals("4", sa.add("2", "2"));
     }
+
+    @Test
+    public void subTestNegativeAsSecond() throws CalculatorException {
+        assertEquals("-5", sa.sub("-8","-3"));
+    }
+
+    @Ignore
+    @Test
+    public void testAddBigNumber1() throws CalculatorException {
+        assertEquals("4000000000", sa.add("2000000000","2000000000"));
+    }
+
+    @Test
+    public void testPrimeNumber() throws CalculatorException {
+        assertTrue(sa.isPrime("2"));
+        assertTrue(sa.isPrime("3"));
+        assertTrue(sa.isPrime("5"));
+        assertTrue(sa.isPrime("7"));
+    }
+
+    @Test
+    public void testNotPrimeNumber() throws CalculatorException {
+        assertFalse(sa.isPrime("4"));
+        assertFalse(sa.isPrime("6"));
+        assertFalse(sa.isPrime("8"));
+    }
+
+
 }
